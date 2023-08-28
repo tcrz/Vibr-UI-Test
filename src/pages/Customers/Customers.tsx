106326518f8d72609360eda1e2c07747d0f14005
“tcrz” <tonybaidoo0@gmail.com>
2023-08-28 19:27:02 +0000

    pagination + layout sizing

--- src/pages/Customers/Customers.tsx
+++ src/pages/Customers/Customers.tsx
@@ -3,7 +3,7 @@ import Heading from "../../components/Heading";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { styled } from '@mui/material';
-import { Button, InputAdornment, OutlinedInput } from '@mui/material';
+import { Button, InputAdornment, OutlinedInput, Pagination } from '@mui/material';
 import { DocumentSVG, SearchSVG } from "../../components/SVGs/CustomSVGs";
 import "../../components/Table/Table.css"
 
@@ -65,46 +65,56 @@ const Customers = () => {
           <AntTab label="Campaigns" />
         </AntTabs>
       </div>
-      <div className="my-4 searchbar flex items-center justify-between flex-wrap borrder-2">
-        <div className="search flex basis-[50%] gap-2 borderr-2 border-red-700">
-          <OutlinedInput className="input-field !rounded-[6px] h-[45px] basis-[80%]" type="text" placeholder="Search customer log by customer name, email address & phone number"
-            startAdornment={<InputAdornment position="start"><SearchSVG /></InputAdornment>}              
-          />
-          <Button variant="outlined" className="basis-[20%] !border-[.1em] !border-[#004741] !text-[#004741] !rounded-[6px] text-[14px]"><p className="font-semibold">Search</p></Button>
+      <section className="borderr border-red-500 h-[65vh]">
+        <div className="my-4 searchbar flex items-center justify-between flex-wrap borrder-2">
+          <div className="search flex basis-[50%] gap-2 borderr-2 border-red-700">
+            <OutlinedInput className="input-field !rounded-[6px] h-[45px] basis-[80%]" type="text" placeholder="Search customer log by customer name, email address & phone number"
+              startAdornment={<InputAdornment position="start"><SearchSVG /></InputAdornment>}              
+            />
+            <Button variant="outlined" className="basis-[20%] !border-[.1em] !border-[#004741] !text-[#004741] !rounded-[6px] text-[14px]"><p className="font-semibold">Search</p></Button>
+          </div>
+          <div className="h-[45px]">
+            <Button variant="contained" startIcon={<DocumentSVG />}
+            className="!bg-[#004741] !text-white !rounded-[6px] text-[14px] !h-full">
+              <p className="font-semibold">Create a campaign</p>
+            </Button>
+          </div>
         </div>
-        <div className="h-[45px]">
-          <Button variant="contained" startIcon={<DocumentSVG />}
-           className="!bg-[#004741] !text-white !rounded-[6px] text-[14px] !h-full">
-            <p className="font-semibold">Create a campaign</p>
-          </Button>
+        <div className="relative h-[55vh] flex flex-col gap-4">
+          <div className="h-[45vh] overflow-y-scroll">
+            <table>
+              <thead>
+                <tr>
+                  <th style={{ textAlign: "left" }}>Campaign Title</th>
+                  <th className="text-left">Description</th>
+                  <th style={{ textAlign: "left" }}>Target Group</th>
+                  <th>Campaign Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  [...Array(6).keys()].map(i => {
+                    return (
+                      <tr>
+                        <td style={{ textAlign: "left" }}><p>Mobile App updates coming soon</p></td>
+                        <td className="text-left"><p>Voice Campaign</p></td>
+                        <td style={{ textAlign: "left" }}><p>All Customers</p></td>
+                        <td><p>Active</p></td>
+                      </tr>
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </div>
+          <div className="h-[10vh]">
+            <Pagination className="absolute right-0"
+                count={10}
+                defaultPage={6}
+            />
+          </div>
         </div>
-      </div>
-      <div className="h-[50%] overflow-y-scroll">
-        <table>
-          <thead>
-            <tr>
-              <th style={{ textAlign: "left" }}>Campaign Title</th>
-              <th className="text-left">Description</th>
-              <th style={{ textAlign: "left" }}>Target Group</th>
-              <th>Campaign Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              [...Array(6).keys()].map(i => {
-                return (
-                  <tr>
-                    <td style={{ textAlign: "left" }}><p>Mobile App updates coming soon</p></td>
-                    <td className="text-left"><p>Voice Campaign</p></td>
-                    <td style={{ textAlign: "left" }}><p>All Customers</p></td>
-                    <td><p>Active</p></td>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
-      </div>
+      </section>
     </section>
   );
 };
