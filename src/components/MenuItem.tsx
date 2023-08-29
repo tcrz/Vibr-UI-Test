437413f149be22ac3a90e3444f8d4c2b1fdcdb06
“tcrz” <tonybaidoo0@gmail.com>
2023-08-29 13:07:27 +0000

    updates

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react'
-import { MenuItemData } from './Navbar'
+import { MenuItemData } from './Nav/Navbar'
 
 type MenuItemProps = MenuItemData
 
 const MenuItem: FC<MenuItemProps> = ({icon, name}) => {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-2 cursor-pointer">
       {icon}
       <p>{name}</p>
     </div>
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from "react";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/Nav/Navbar";
 
 type LayoutProps = {
   children: ReactNode;
