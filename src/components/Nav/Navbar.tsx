a934f1f606d75f9a6130be3eee458336485121f3
“tcrz” <tonybaidoo0@gmail.com>
2023-08-29 15:10:11 +0000

    disable drawer click event on large screens

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -57,7 +57,7 @@ const Navbar = () => {
     <>
     <nav className="navbar h-[10vh] bg-white flex items-center px-20 border-b border-[rgba(204, 207, 206, 0.24)] sm:h-[11.5vh]">
       <div className="bgg-yellow-300 w-full h-full flex items-center justify-around sm:justify-between">
-        <img src={logo} width="150" height="150" alt="buuk-me-now logo" onClick={toggleDrawer("left", true)}/>
+        <img src={logo} width="150" height="150" alt="buuk-me-now logo" className="lg:pointer-events-none" onClick={toggleDrawer("left", true)}/>
         <div className="nav-menu borderr border-green-300 hidden items-center gap-6 text-[#808885] lg:flex">
           {items.map((item) => (
             <MenuItem icon={item.icon} name={item.name} />
