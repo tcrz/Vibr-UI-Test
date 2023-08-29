7677b90c9b37ff8602ff4a9be62c1c6a1652a7e3
“tcrz” <tonybaidoo0@gmail.com>
2023-08-29 14:51:16 +0000

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import Customers from "./pages/Customers/Customers";
 import Layout from "./pages/Layout/Layout";
 
 function App() {
-
   return (
   <Layout>
     <Customers />
