6987eed75690bca18da8813a64f1702e857ec5ad
“tcrz” <tonybaidoo0@gmail.com>
2023-08-29 12:05:24 +0000

    random data file to test pagination

---
+++ src/pages/Customers/data.tsx
@@ -0,0 +1,95 @@
+import { CampaignData } from "./Customers";
+
+export const data: CampaignData[] = [
+  {
+    title: "Project A",
+    description: "Description for Project A",
+    target: "All customers",
+    status: "Active",
+  },
+  {
+    title: "Project B",
+    description: "Description for Project B",
+    target: "Some customers",
+    status: "Active",
+  },
+  {
+    title: "Project C",
+    description: "Description for Project C",
+    target: "Owners",
+    status: "Active",
+  },
+  // Add more items
+  {
+    title: "Project D",
+    description: "Description for Project D",
+    target: "All customers",
+    status: "Active",
+  },
+  {
+    title: "Project E",
+    description: "Description for Project E",
+    target: "Owners",
+    status: "Active",
+  },
+  {
+    title: "Project F",
+    description: "Description for Project F",
+    target: "Some customers",
+    status: "Active",
+  },
+  {
+    title: "Project G",
+    description: "Description for Project G",
+    target: "All customers",
+    status: "Active",
+  },
+  {
+    title: "Project H",
+    description: "Description for Project H",
+    target: "Some customers",
+    status: "Active",
+  },
+  {
+    title: "Project I",
+    description: "Description for Project I",
+    target: "Owners",
+    status: "Active",
+  },
+  {
+    title: "Project J",
+    description: "Description for Project J",
+    target: "All customers",
+    status: "Active",
+  },
+  {
+    title: "Project K",
+    description: "Description for Project K",
+    target: "Owners",
+    status: "Active",
+  },
+  {
+    title: "Project L",
+    description: "Description for Project L",
+    target: "Some customers",
+    status: "Active",
+  },
+  {
+    title: "Project M",
+    description: "Description for Project M",
+    target: "All customers",
+    status: "Active",
+  },
+  {
+    title: "Project N",
+    description: "Description for Project N",
+    target: "Some customers",
+    status: "Active",
+  },
+  {
+    title: "Project O",
+    description: "Description for Project O",
+    target: "Owners",
+    status: "Active",
+  },
+];
