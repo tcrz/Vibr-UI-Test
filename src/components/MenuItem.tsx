8ff2c3d47d230e479a0939de7434dab3a4cbd482
“tcrz” <tonybaidoo0@gmail.com>
2023-08-29 16:04:26 +0000

    active tab styling + type updates

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react'
 import { MenuItemData } from './Nav/Navbar'
 
-type MenuItemProps = MenuItemData
+type MenuItemProps = MenuItemData & { isActive: boolean }
 
-const MenuItem: FC<MenuItemProps> = ({icon, name}) => {
+const MenuItem: FC<MenuItemProps> = ({icon, name, isActive}) => {
   return (
-    <div className="flex items-center gap-2 cursor-pointer">
+    <div className={`flex items-center gap-2 cursor-pointer ${isActive ? "bg-[#F3F4F6] text-black font-semibold" : ""} p-2 rounded-xl`}>
       {icon}
       <p>{name}</p>
     </div>
