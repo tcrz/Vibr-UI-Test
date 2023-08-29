c3c3e6784974f159637f114c3994690973a3718b
“tcrz” <tonybaidoo0@gmail.com>
2023-08-29 00:08:01 +0000

    modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,26 @@
+import { FC, ReactNode } from "react"
+import "./Modal.css"
+
+type ModalProps = {
+  open: Boolean,
+  icon: ReactNode,
+  title: string,
+  setOpen: (bool:boolean) => void,
+  children: ReactNode
+}
+
+const Modal:FC<ModalProps> = ({open, setOpen, title, icon, children}) => {
+  return (
+    <div className="modal-backdrop"
+      onClick={() => open ? setOpen(false) : () => {}}
+      style={{ display: open ? "flex" : "none" }}
+      >
+      <div className="modal rounded-md p-[20px] w-5/6 h-[90%] sm:w-2/5" onClick={(e) => e.stopPropagation()}>
+        <h5 className="flex items-center text-black text-[20px] font-semibold gap-2"><span>{icon}</span>{title}</h5>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default Modal
